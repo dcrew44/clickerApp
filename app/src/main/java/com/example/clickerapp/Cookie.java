0ef0d3e63f6c097205c14863432603dd57b354b3
dcrew44 <haydenbarolette@gmail.com>
2023-04-16 23:36:11 -0700

    Adding finish

--- app/src/main/java/com/example/clickerapp/Cookie.java
+++ app/src/main/java/com/example/clickerapp/Cookie.java
@@ -5,9 +5,14 @@ public class Cookie {
     public long cClicks; //current clicks
     public long cPS; //current clicks per second
     public long cPC; //current clicks per click
+    public int clickLevel; //level of clicker
     public long autoClickers; //number of autoclickers
     public int autoClickerLevel; //level of autoclickers
     public int autoClickRate; //rate of autoclickers
+    public long autoClickerCost; //cost of autoclickers
+    public long autoClickerUpgradeCost; //cost of upgrading autoclickers
+    public long upgradeCost; //cost of upgrading clicker
+    private final long MULTIPLIER = (long) 1.2;
     public Cookie() {
         tClicks = 0;
         cClicks = 0;
@@ -15,24 +20,52 @@ public class Cookie {
         cPC = 1;
         autoClickerLevel = 1;
         autoClickRate = 1000;
+        autoClickerCost = 10;
+        autoClickerUpgradeCost = 10;
+        upgradeCost = 10;
+        clickLevel = 1;
     }
     public void click() {
         tClicks+=cPC;
         cClicks+=cPC;
     }
-    public void addAutoClicker() {
-        autoClickers++;
+    public boolean addAutoClicker() {
+        if (autoClickerCost <= cClicks) {
+            cClicks -= autoClickerCost;
+            autoClickers++;
+            autoClickerCost = (long) (autoClickerCost * Math.pow(MULTIPLIER, autoClickers));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean upgradeAutoClicker() {
+        if (autoClickerUpgradeCost <= cClicks) {
+            cClicks -= autoClickerUpgradeCost;
+            autoClickRate /= 2;
+            autoClickerUpgradeCost = (long) (autoClickerUpgradeCost * Math.pow(MULTIPLIER, autoClickerLevel));
+            autoClickerLevel++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean upgradeClicker() {
+        if (upgradeCost <= cClicks) {
+            cClicks -= upgradeCost;
+            cPC += Math.pow(1.4, clickLevel);
+            upgradeCost = (long) (upgradeCost * Math.pow(MULTIPLIER, cPC));
+            clickLevel++;
+            return true;
+        } else {
+            return false;
+        }
     }
     public void update() {
         long autoCPC = autoClickerLevel * autoClickers;
         tClicks += autoCPC;
         cClicks += autoCPC;
-    }
-    public void upgradeAutoClicker() {
-        autoClickerLevel++;
-    }
-    public void upgradeClicker() {
-        cPC++;
     }
     public String getClicks() {
         return Long.toString(cClicks);
@@ -41,4 +74,14 @@ public class Cookie {
     public int getAutoClickRate() {
         return autoClickRate;
     }
+    public String getAutoClickerCost() {
+        return Long.toString(autoClickerCost);
+    }
+    public String getAutoClickerUpgradeCost() {
+        return Long.toString(autoClickerUpgradeCost);
+    }
+    public String getUpgradeCost() {
+        return Long.toString(upgradeCost);
+    }
+
 }
--- app/src/main/java/com/example/clickerapp/MainActivity.java
+++ app/src/main/java/com/example/clickerapp/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.view.LayoutInflater;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,14 +23,45 @@ public class MainActivity extends AppCompatActivity {
     public ImageView cookieImage;
     public Looper looper;
     public Thread backgroundThread;
+
+    public Button upgradeClicker;
+    public Button upgradeAutoClicker;
+    public Button addAutoClicker;
+    public TextView upgradeCost;
+    public TextView autoClickerCost;
+    public TextView autoClickerUpgradeCost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         cookie = new Cookie();
+
         cookieCount = findViewById(R.id.clickCount);
         cookieCount.setText(cookie.getClicks());
+        upgradeClicker = (Button) findViewById(R.id.clickUpgradeButton);
+        upgradeAutoClicker = (Button) findViewById(R.id.upgradeAutoButton);
+        addAutoClicker = (Button) findViewById(R.id.cursorBuyButton);
+        upgradeCost = (TextView) findViewById(R.id.upgradeCostText);
+        autoClickerCost = (TextView) findViewById(R.id.aBuyText);
+        autoClickerUpgradeCost = (TextView) findViewById(R.id.aUpgradeText);
+
+        upgradeClicker.setOnClickListener(v -> {
+            if (cookie.upgradeClicker()) {
+                upgradeCost.setText("Cost: "+cookie.getUpgradeCost());
+            }
+        });
+        upgradeAutoClicker.setOnClickListener(v -> {
+            if (cookie.upgradeAutoClicker()) {
+                autoClickerUpgradeCost.setText("Cost: "+cookie.getAutoClickerUpgradeCost());
+            }
+        });
+        addAutoClicker.setOnClickListener(v -> {
+            if (cookie.addAutoClicker()) {
+                autoClickerCost.setText("Cost: "+cookie.getAutoClickerCost());
+                newAutoClicker();
+            }
+        });
         container = findViewById(R.id.container);
         inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
         cookieImage = (ImageView) inflater.inflate(R.layout.cookie_image, null);
@@ -41,7 +74,9 @@ public class MainActivity extends AppCompatActivity {
         backgroundThread.start();
         looper = Looper.getMainLooper();
     }
-
+    //add mini auto clicker inflating a cursor image around the cookie
+    private void newAutoClicker() {
+    }
     private Runnable update = new Runnable() {
         @Override
         public void run() {
@@ -61,6 +96,23 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void handleMessage(Message msg) {
             if (msg.what == 1) {
+                cookieImage.animate()
+                        .scaleX(1.2f)
+                        .scaleY(1.2f)
+                        .setDuration(500)
+                        .setInterpolator(new AccelerateDecelerateInterpolator())
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                cookieImage.animate()
+                                        .scaleX(1f)
+                                        .scaleY(1f)
+                                        .setDuration(500)
+                                        .setInterpolator(new AccelerateDecelerateInterpolator())
+                                        .start();
+                            }
+                        })
+                        .start();
             }
             cookieCount.setText(cookie.getClicks());
         }
